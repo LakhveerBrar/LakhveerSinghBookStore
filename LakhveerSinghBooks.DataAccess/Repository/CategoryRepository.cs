9fa0b2f434706f6f8e69712512b1718259758f31
W0834082 <W0834082@ADL-A1201H-19>
2023-11-06 19:06:47 -0500

    Add two new classes in repository and modify it

---
+++ LakhveerSinghBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using LakhveerSinghBooks.DataAccess.Repository.IRepository;
+using LakhveerSinghBooks.Models;
+using LakhveerSinghBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LakhveerSinghBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
