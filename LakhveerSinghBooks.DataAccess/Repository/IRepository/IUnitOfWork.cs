5321e9d1f67f78b3091bd7db738fd9d7ef16eb77
W0834082 <W0834082@ADL-A1201I-22>
2023-11-20 14:37:20 -0500

    add new interface class Name IUnitOfWork

---
+++ LakhveerSinghBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LakhveerSinghBooks.DataAccess.Repository.IRepository
+{
+
+    interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get;  }
+        ISP_Call SP_Call { get; }
+       
+    }
+}
