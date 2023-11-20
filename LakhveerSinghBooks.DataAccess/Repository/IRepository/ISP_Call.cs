e8acf17a7194b4d981b42acc0936da79fdf53f91
W0834082 <W0834082@ADL-A1201I-22>
2023-11-20 14:04:04 -0500

    impliment the codes in the interface class

--- LakhveerSinghBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ LakhveerSinghBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,10 @@ namespace LakhveerSinghBooks.DataAccess.Repository.IRepository
 {
     interface ISP_Call : IDisposable
     {
-        T Single<T>(string procedurename, DynamicParameters)
+        T Single<T>(string procedurename, DynamicParameters param = null);
+        void Execute(string procedurename, DynamicParameters param = null);
+        T OneRecord<T>(string procedurename, DynamicParameters param = null);
+        IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
     }
 }
